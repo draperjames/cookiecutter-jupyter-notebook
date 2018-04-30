d783eee39cab025a6faa93af34c024319fc6b485
James Draper <james.draper@duke.edu>
2018-04-30 13:56:56 -0400

    inserting date

--- hooks/post_get_project.py
+++ hooks/post_get_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+from datetime import datetime
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -8,5 +9,19 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
-# if __name__ == '__main__':
-#     remove_file("README.md")
+def replace_contents(filename, what, replacement):
+    filename = os.path.join(PROJECT_DIRECTORY, filepath)
+
+    with open(filename) as fh:
+        target_file = fh.read()
+
+    with open(filename, 'w') as fh:
+        fh.write(target_file.replace(what, replacement))
+
+
+if __name__ == "__main__":
+
+    if '{{ cookiecutter.date_created }}' == 'now':
+        now = datetime.now()
+
+        replace_contents('{{ cookiecutter.project_slug }}', '<DATE>', now.strftime("%Y-%m-%d"))
